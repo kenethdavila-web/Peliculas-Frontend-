4fa480b3a945a5ee950a56e34b7f693bdbe5be48
kenethdavila-web <keneth.davila@est.iudigital.edu.co>
2026-03-25 23:19:19 -0500

    Aplicación y index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Generos from "./pages/Generos";
+import Directores from "./pages/Directores";
+import Productoras from "./pages/Productoras";
+import Tipos from "./pages/Tipos";
+import Media from "./pages/Media";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/generos" component={Generos} />
+        <Route path="/directores" component={Directores} />
+        <Route path="/productoras" component={Productoras} />
+        <Route path="/tipos" component={Tipos} />
+        <Route path="/media" component={Media} />
+      </Switch>
+
+    </Router>
   );
 }
 
